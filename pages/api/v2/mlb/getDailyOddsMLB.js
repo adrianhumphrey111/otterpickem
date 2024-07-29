f26e3f72eb16e061dbae510b6c13b58186b3f7c1
adrianhumphrey111 <adrianhumphrey374@gmail.com>
2024-07-29 15:51:38 -0500

    Added date parameter validation and updated the API URL to use the provided date.

--- pages/api/v2/mlb/getDailyOddsMLB.js
+++ pages/api/v2/mlb/getDailyOddsMLB.js
@@ -2,8 +2,13 @@ import { makeDelayedApiCall } from '../../../../utils/apiUtils';
 
 export default async function handler(req, res) {
   try {
-    const oddsData = await getDailyOddsMLB();
+    const { date } = req.query;                                                                            
+     if (!date) {                                                                                           
+       return res.status(400).json({ error: 'Date parameter is required' });                                
+     }                                                                                                      
+    const oddsData = await getDailyOddsMLB(date); 
     res.status(200).json(oddsData);
+
   } catch (error) {
     console.error('Error fetching MLB odds:', error);
     res.status(500).json({ error: 'Failed to fetch MLB odds' });
@@ -11,7 +16,7 @@ export default async function handler(req, res) {
 }
 
 async function getDailyOddsMLB(date) {
-  const url = `https://api.sportradar.com/oddscomparison-ust1/en/us/sports/sr:sport:3/{date}/schedule.json`;
+  const url = `https://api.sportradar.com/oddscomparison-ust1/en/us/sports/sr:sport:3/${date}/schedule.json`;
   const response = await makeDelayedApiCall(url, {}, 0);
 
   const mlbEvents = response.sport_events.filter(event => event.tournament.name === "MLB");
