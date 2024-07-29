8e3f9d10d7da330e9795c61fadc6be9466c02c77
adrianhumphrey111 <adrianhumphrey374@gmail.com>
2024-07-28 23:01:18 -0500

    Renamed the `makeApiCall` function to `makeDelayedApiCall` to better describe its functionality.

--- utils/apiUtils.js
+++ utils/apiUtils.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-export async function makeApiCall(url, params, delayMs) {
+export async function makeDelayedApiCall(url, params, delayMs) {
   await delay(delayMs);
   const response = await axios.get(url, {
     params: { ...params, api_key: process.env.SPORTS_RADAR_API_KEY },
