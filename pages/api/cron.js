50acfe52d2e6fd0a96033f9c443fe2265e23fe54
adrianhumphrey111 <adrianhumphrey374@gmail.com>
2024-07-30 13:15:47 -0500

    Fix cron job title

--- pages/api/cron.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(request, response) {
-    const result = await fetch(
-      'http://worldtimeapi.org/api/timezone/America/Chicago',
-    );
-    const data = await result.json();
-   
-    return response.json({ datetime: data.datetime });
-  }
-  
