fbd8462560119aeb31ac78c1efa2a3a1eadcb79c
adrianhumphrey111 <adrianhumphrey374@gmail.com>
2024-08-01 17:12:13 -0500

    fix: Update the API URL in the getGameDetails function

--- app/game/[gameId]/page.tsx
+++ app/game/[gameId]/page.tsx
@@ -1,7 +1,7 @@
 import { EvaluatedGame } from '../../types';
 
 async function getGameDetails(gameId: string) {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v2/mlb/getGameDetails?gameId=${gameId}`, { cache: 'no-store' });
+  const response = await fetch(`localhost:3000/api/v2/mlb/getGameDetails?gameId=${gameId}`, { cache: 'no-store' });
   if (!response.ok) {
     throw new Error('Failed to fetch game details');
   }
