e1e839016a0973c1e3076ebc1713da4702115adb
adrianhumphrey111 <adrianhumphrey374@gmail.com>
2024-08-09 14:10:29 -0500

    feat: Refactor getTeamStatistics API endpoint to fetch MLB league leaders

--- pages/api/v2/mlb/getTeamStatistics.js
+++ pages/api/v2/mlb/getTeamStatistics.js
@@ -1,21 +1,29 @@
 import { makeDelayedApiCall } from '../../../../utils/apiUtils';
 
 export async function getTeamStatistics(teamId, season = '2024', competition = 'REG') {
-  const url = `https://api.sportradar.com/mlb/trial/v7/en/seasons/${season}/${competition}/teams/${teamId}/statistics.json`;
-  return await makeDelayedApiCall(url, {}, 1500);
+  const url = `https://api.sportradar.com/mlb/trial/v7/en/seasons/2024/REG/leaders/statistics.json`;
+  return await makeDelayedApiCall(url, {}, 0);
 }
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
-      const { teamId, season, competition } = req.query;
       
-      if (!teamId) {
-        return res.status(400).json({ error: 'Team ID is required' });
+      const teamStats = await getTeamStatistics()
+      const mlbLeaders = teamStats.leagues.filter( l => l.alias === "MLB") || {}
+      const {batting_average, runs_scored, doubles, triples, home_runs, runs_batted_in, stolen_bases, hits} = mlbLeaders.hitting
+      let truncatedTeamStats = {
+        battingAverage: batting_average.teams.map( t => ({name: t.name, rank: t.rank, name: t.name, avg: t.avg})),
+        runsScored: runs_scored.teams.map( t => ({name: t.name, rank: t.rank, name: t.name, runs: t.runs})),
+        doubles: doubles.teams.map( t => ({name: t.name, rank: t.rank, name: t.name, doubles: t.doubles})),
+        triples: triples.teams.map( t => ({name: t.name, rank: t.rank, name: t.name, triples: t.triples})),
+        homeRuns: home_runs.teams.map( t => ({name: t.name, rank: t.rank, name: t.name, homeRuns: t.hr})),
+        runsBattedIn: runs_batted_in.teams.map( t => ({name: t.name, rank: t.rank, name: t.name, rbis: t.rbi})),
+        hits: hits.teams.map( t => ({name: t.name, rank: t.rank, name: t.name, hits: t.h})),
+        stolenBases: stolen_bases.teams.map( t => ({name: t.name, rank: t.rank, name: t.name, stolenBases: t.sb})),
       }
-
-      const teamStats = await getTeamStatistics(teamId, season, competition);
-      res.status(200).json(teamStats.statistics);
+      = mlbLeaders.hitting
+      res.status(200).json(truncatedTeamStats);
     } catch (error) {
       console.error('Error fetching team statistics:', error);
       res.status(500).json({ error: 'Internal Server Error' });
